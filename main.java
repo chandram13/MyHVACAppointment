25355c923f97c5715bcf1c3d4e65086aba90a17c
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-15 09:42:04 -0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,26 @@
+// Marvish Chandra
+
+public class clientRequest{
+    int issueid;
+    String technicianName;
+    String clientName;
+    String heatingIssue;
+    String ventilationIssue;
+    String airIssue;
+    String coolingIssue;
+    int timeArrival;
+    int expectedCompletion;
+
+    static void HVACappointment(String[] args){
+        int issueid = int;
+        string technicianName = string;
+        string clientName = string;
+        string heatingIssue = string;
+        string ventilationIssue = string;
+        string airIssue = string;
+        string coolingIssue = string;
+        int timeArrival = int;
+        int expectedCompletion = int;
+    }
+}
+
